c16a80f43ef2a1ce9495feb17ffa9ea808a061be
Jhonattan Lino <jhonattanlino@outlook.com>
2023-10-14 23:34:09 -0300

    Ajuste do modelo

--- Lista.R
+++ Lista.R
@@ -1,8 +1,9 @@
 #### Lista modelos lineares generalizados ####
 
 # Bibliotecas
-library('readxl')
-library('faraway')
+library(faraway)
+library(performance)
+library(statmod)
 
 ?faraway::wbca
 
@@ -13,3 +14,29 @@ cancer$Class <- factor(cancer$Class, levels = c(0, 1), labels = c('Benigno', 'Ma
 cancer_train <- cancer[0:500,]
   
 cancer_test <- cancer[501:681,]
+
+tabela <- with(cancer, table(BNucl, Class))
+round(prop.table(tabela))
+
+par(las = 1, mar = c(5,4.5,2,2), mfrow = c(1,2), cex = 1.25)
+boxplot(UShap ~ Class, data = cancer, col = c('blue', 'red'), ylab = 'UShap')
+boxplot(BNucl ~ Class, data = cancer, col = c('blue', 'red'), ylab = 'BNucl')
+
+#### Regressão Logistica ####
+
+ajuste <- glm(Class ~., data = cancer_train, family = binomial(link = 'logit'))
+options(scipen = 5)
+sumary(ajuste)
+
+plot(ajuste)
+
+
+#### Analise de residuos ####
+
+residuos <- statmod::qres.binom(ajuste)
+ajustados <- predict(ajuste)
+
+?par
+par(las=0, mar=c(5, 4, 4, 2))
+plot(residuos ~ ajustados, col = 'blue', xlab = 'Valores Ajustados', ylab='Resíduos')
+
